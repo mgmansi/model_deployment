b014f2c34e8b8f4f1422e2fd9ed97952b77ab11c
mgmansi <mansigupta1nine@gmail.com>
2020-06-18 23:33:34 +0530

    changes in config

--- regression_model/regression_model/config/config.py
+++ regression_model/regression_model/config/config.py
@@ -1,3 +1,16 @@
+import pathlib
+import regression_model
+import pandas as pd
+
+pd.options.display.max_rows = 10
+pd.options.display.max_columns = 10
+
+
+PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
+TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
+DATASET_DIR = PACKAGE_ROOT / "datasets"
+
+
 # ====   PATHS ===================
 
 TRAINING_DATA_FILE = "titanic.csv"
@@ -13,3 +26,6 @@ CATEGORICAL_VARS = ['sex', 'cabin', 'embarked', 'title']
 NUMERICAL_VARS = ['age', 'fare']
 
 CABIN = 'cabin'
+
+PIPELINE_NAME = "logistic_regression"
+PIPELINE_SAVE_FILE = f"{PIPELINE_NAME}_output_v"
